c0c0c078a127e5433ef8be2ad0ee29ec97f04b76
David Sanders <david.wm.sanders@gmail.com>
2021-08-29 06:13:20 +0100

    Tidy

--- py/start.py
+++ py/start.py
@@ -53,7 +53,6 @@ def _consume_prompt(proc):
     # consume the prompt marker
     prompt_marker = proc.stdout.read(1)
     if prompt_marker != ">":
-        # print(f"bad error = {proc.stdout.readline()}")
         raise Exception(f"prompt marker read got unexpected '{prompt_marker}' :/")
 
 
@@ -61,7 +60,7 @@ def wait_for_server_load(proc):
     _spinner_steps = itertools.cycle(["/", "-", "\\", "|"])
     while True:
         # read a line from rwr server stdout
-        output_line = proc.stdout.readline()
+        output_line = proc.stdout.readline().lstrip(">")
         # strip the line for easier processing
         stripped_line = output_line.strip()
         # print(f"{stripped_line=!r}")
@@ -73,8 +72,6 @@ def wait_for_server_load(proc):
             break
         else:
             print(stripped_line)
-    # consume the prompt marker
-    # _consume_prompt(proc)
     return True
 
 
@@ -89,7 +86,6 @@ if __name__ == '__main__':
         sys.exit(1)
 
     print("Reading package config...")
-    # with PKG_CFG.open(mode="r", encoding="utf-8") as f:
     pkg_cfg = PackageConfig.from_xml_file(PKG_CFG)
     print(f"Package name: {pkg_cfg.name}, script: {pkg_cfg.campaign_entry_script}")
 
@@ -112,67 +108,12 @@ if __name__ == '__main__':
         # write the start script command to rwr server stdin
         rwr_serv.stdin.write(f"start_script {pkg_cfg.campaign_entry_script} {path_to_package}\n")
         rwr_serv.stdin.flush()
-        # consume the next prompt
-        # _consume_prompt(rwr_serv)
         # wait again as the server now loads from overlays set in the script
         wait_for_server_load(rwr_serv)
         print(f"Package script loaded - starting server?!")
         # write the start server command to rwr server stdin
-
+        # TODO: this server stuff^
     except KeyboardInterrupt:
         print("Ctrl-C detected, shutting down!")
         rwr_serv.kill()
 
-    #
-    # ctx = Context.INIT
-    # script_started = False
-    # try:
-    #     while True:
-    #         if ctx == Context.INIT:
-    #             # get lines, render nice, wait for `Game loaded` before proceeding
-    #             output_line = rwr_serv.stdout.readline()
-    #             stripped_line = output_line.strip()
-    #             if stripped_line.startswith("Loading"):
-    #                 print(f"{_s} Loading...", end="\r")
-    #                 _s = next(_spinner_steps)
-    #             elif stripped_line == "Game loaded":
-    #                 # print(f"Game loaded - starting script '{pkg_cfg.campaign_entry_script}'...")
-    #                 # rwr_serv.stdin.write("help\n")
-    #                 # rwr_serv.stdin.flush()
-    #                 ctx = Context.START_SCRIPT
-    #         elif ctx == Context.START_SCRIPT:
-    #             if not script_started:
-    #                 print(f"Game loaded - starting script '{pkg_cfg.campaign_entry_script}'...")
-    #                 rwr_serv.stdin.write(f"start_script {pkg_cfg.campaign_entry_script} {path_to_package}\n")
-    #                 rwr_serv.stdin.flush()
-    #                 script_started = True
-    #             # get lines, render nice, wait for `Game loaded` before proceeding
-    #             output_line = rwr_serv.stdout.readline()
-    #             stripped_line = output_line.strip()
-    #             if stripped_line.startswith("Loading"):
-    #                 print(f"{_s} Loading...", end="\r")
-    #                 _s = next(_spinner_steps)
-    #             elif stripped_line == "Game loaded":
-    #                 # TODO: add step to START_SERVER here
-    #                 ctx = Context.START_SERVER
-    #         elif ctx == Context.START_SERVER:
-    #             print("Starting server...")
-    #             # TODO: do it!
-    #             ctx = Context.PRINT_STATUS
-    #         elif ctx == Context.PRINT_STATUS:
-    #             print("Collecting status info from rwr server...")
-    #             # TODO: collect some status
-    #             ctx = Context.WAIT
-    #         elif ctx == Context.WAIT:
-    #             output_line = rwr_serv.stdout.readline()
-    #             # stripped_line = output_line.strip()
-    #             print(output_line, end="")
-    #         else:
-    #             raise Exception("invalid context :/")
-    #         # else:
-    #         #     print(output_line, end="")
-    # except KeyboardInterrupt:
-    #     print("Ctrl-C detected, shutting down!")
-    #     rwr_serv.kill()
-    #     # rwr_serv.stdin.write("quit\n")
-    #     # rwr_serv.stdin.flush()
