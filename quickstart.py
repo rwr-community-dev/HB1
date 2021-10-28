4bfb5b362edbd20f21a6ef597273d751204132fe
David Sanders <david.wm.sanders@gmail.com>
2021-10-28 23:31:06 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -5,7 +5,7 @@ import pathlib
 
 SCRIPT_DIR = pathlib.Path(__file__).parent
 RWR_STEAM_URI = "steam://rungameid/270150//"
-PKG_PATH = "media/packages/CB7"
+PKG_PATH = "media/packages/HB1"
 VANILLA_MAPS_PATH_PREFIX = "media/packages/vanilla/maps/"
 
 if __name__ == '__main__':
